225a315f7e247df4ecbb8bcfaf47852fb4ead3ea
Patrick-Mesey <Patrick.mesey@gmail.com>
2022-10-18 21:59:32 -0500

    Updated Array
    Added groceries as a list instead of individual items. Simplified the output.

--- grocery.ts
+++ grocery.ts
@@ -8,17 +8,11 @@ class Grocery {
     }
 }
 
-interface Item {
-    name: string;
-    quantity: number;
-}
+let list_of_groceries = [
+    new Grocery("Milk", 1), 
+    new Grocery("Bread", 5),
+    new Grocery("Eggs", 12)
+]
 
-function groceryList(item: Item) {
-    return item.quantity + " " + item.name;
-}
 
-let item1 = new Grocery("Milk", 1);
-let item2 = new Grocery("Bread", 5);
-let item3 = new Grocery("Eggs", 12);
-
-document.body.textContent = "You need: " + groceryList(item1) + ", " + groceryList(item2) + ", " + groceryList(item3);
+document.body.textContent = JSON.stringify(list_of_groceries);
